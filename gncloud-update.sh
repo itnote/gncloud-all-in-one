cb8b8e996c0e7793d1e481b6c76e356921e3b912
CHONAMHYUN <erobeta@gmail.com>
2017-03-17 15:04:36 +0900

    update 추가

---
+++ gncloud-update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull gncloud/gncloud-mysql
+docker pull gncloud/gncloud-web
+docker pull gncloud/gncloud-scheduler
+docker pull gncloud/gncloud-manager
+docker pull gncloud/gncloud-docker
+docker pull gncloud/gncloud-hyperv
+docker pull gncloud/gncloud-kvm
+docker pull gncloud/gncloud-registry
